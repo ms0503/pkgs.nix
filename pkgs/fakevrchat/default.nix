89c4ae0c00dc942d0015482495e7e8b084c3b32f
Sora Tonami <ms0503@outlook.com>
2025-08-08 02:56:27 +0900

    feature: fakevrchat: add steam-run to runtime

--- pkgs/fakevrchat/default.nix
+++ pkgs/fakevrchat/default.nix
@@ -6,18 +6,21 @@
   lib,
   proton-path ? "${compat-client-install-path}/steamapps/common/Proton - Experimental/proton",
   stdenv,
+  steam-run,
   vrchat-exe-path ? "${compat-client-install-path}/steamapps/common/VRChat",
 }:
 assert lib.assertMsg (
   compat-client-install-path != ""
 ) "at least, you must specify compat-client-install-path";
 stdenv.mkDerivation {
+  buildInputs = [
+    steam-run
+  ];
   buildPhase = ''
     runHook preBuild
     g++ -O2 -o FakeVRChat.exe FakeVRChat.cpp
     runHook postBuild
   '';
-  dontUnpack = true;
   installPhase = ''
     runHook preInstall
     install -Dm555 FakeVRChat.exe "$out/opt/FakeVRChat/bin/FakeVRChat.exe"
@@ -35,17 +38,25 @@ stdenv.mkDerivation {
   };
   patchPhase = ''
     runHook prePatch
-    sed "$src" >FakeVRChat.cpp \
+    sed -i FakeVRChat.cpp \
       -e 's|^	std::string compat_data_path .*$|	std::string compat_data_path = "${compat-data-path}";|' \
       -e 's|^	std::string compat_client_install_path .*$|	std::string compat_client_install_path = "${compat-client-install-path}";|' \
       -e 's|^	std::string proton_path .*$|	std::string proton_path = "${proton-path}";|' \
       -e 's|^	std::string vrchat_exe_path .*$|	std::string vrchat_exe_path = "${vrchat-exe-path}";|'
     runHook postPatch
   '';
+  patches = [
+    ./add-steam-run.patch
+  ];
   pname = "fakevrchat";
   src = fetchurl {
     inherit hash;
     url = "https://software.tlfoxhuman.net/categories/small-code/FakeVRChat.exe/FakeVRChat.cpp";
   };
-  version = "1.0.0";
+  unpackPhase = ''
+    runHook preUnpack
+    cp "$src" FakeVRChat.cpp
+    runHook postUnpack
+  '';
+  version = "1.0.1";
 }
