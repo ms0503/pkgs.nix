cbb7176e66342b3b3f25c91cdbb73870b48fe285
Sora Tonami <ms0503@outlook.com>
2026-05-03 15:22:27 +0900

    refactor: simplify process of adding external packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,4 @@
-_: {
+{
   perSystem =
     {
       inputs',
@@ -19,6 +19,22 @@ _: {
         fetchurl
         makeRustPlatform
         ;
+      externalPackages = [
+        "awww-bing"
+        "colortool"
+        "generatehex"
+        "getcodepoint"
+        "getemoji"
+        "mc-mod-downloader"
+        "skel"
+        "unicodeescape"
+        "urlencode"
+      ];
+      generateExternalPackages = packages: lib.genAttrs packages (name: inputs'.${name}.packages.default);
+      rustPlatform = makeRustPlatform {
+        cargo = inputs'.fenix.packages.latest.toolchain;
+        rustc = inputs'.fenix.packages.latest.toolchain;
+      };
       sources = import ../_sources/generated.nix {
         inherit
           dockerTools
@@ -29,84 +45,69 @@ _: {
       };
     in
     {
-      packages =
-        let
-          rustPlatform = makeRustPlatform {
-            cargo = inputs'.fenix.packages.latest.toolchain;
-            rustc = inputs'.fenix.packages.latest.toolchain;
-          };
-        in
-        {
-          alcom = builtins.warn "github:ms0503/pkgs.nix#alcom is deprecated. Please use nixpkgs#alcom instead." alcom;
-          awww-bing = inputs'.awww-bing.packages.default;
-          colortool = inputs'.colortool.packages.default;
-          discord-canary-wayland = callPackage ./discord-canary-wayland { };
-          ds4pairer = callPackage ./ds4pairer {
-            source = sources.ds4pairer;
-          };
-          fakevrchat = callPackage ./fakevrchat { };
-          generatehex = inputs'.generatehex.packages.default;
-          getcodepoint = inputs'.getcodepoint.packages.default;
-          getemoji = inputs'.getemoji.packages.default;
-          git-vrc = callPackage ./git-vrc {
-            inherit rustPlatform;
-            cargoHash = "sha256-/vO8xkD0uW0kqF8RzvAw2/TAvmDI5N8GZD0f6S6lY+M=";
-            source = sources.git-vrc;
-          };
-          mc-mod-downloader = inputs'.mc-mod-downloader.packages.default;
-          microsoft-edge-dev = callPackage ./microsoft-edge-dev { };
-          microsoft-edge-dev-wayland = callPackage ./microsoft-edge-dev-wayland {
-            inherit (self'.packages) microsoft-edge-dev;
-          };
-          microsoft-edit = callPackage ./microsoft-edit {
-            inherit rustPlatform;
-            cargoHash = "sha256-U8U70nzTmpY6r8J661EJ4CGjx6vWrGovu5m25dvz5sY=";
-            source = sources.microsoft-edit;
-          };
-          noto-fonts-cjk-sans-non-variable = callPackage ./noto-fonts-cjk-sans-non-variable {
-            source = sources.noto-cjk-sans;
-          };
-          noto-fonts-cjk-serif-non-variable = callPackage ./noto-fonts-cjk-serif-non-variable {
-            source = sources.noto-cjk-serif;
-          };
-          noto-fonts-non-variable = callPackage ./noto-fonts-non-variable {
-            source = sources.noto-fonts;
-          };
-          proton-ge-rtsp-bin = callPackage ./proton-ge-rtsp-bin {
-            source = sources.proton-ge-rtsp-bin;
-          };
-          skel = inputs'.skel.packages.default;
-          slack-wayland = callPackage ./slack-wayland { };
-          spotify-wayland = callPackage ./spotify-wayland { };
-          unicodeescape = inputs'.unicodeescape.packages.default;
-          unzip-unicode = callPackage ./unzip-unicode { };
-          urlencode = inputs'.urlencode.packages.default;
-          urxvt-wrapper = callPackage ./urxvt-wrapper { };
-          walland = callPackage ./walland {
-            source = sources.walland;
-          };
-          wezimgcat-wrapper = callPackage ./wezimgcat-wrapper {
-            inherit rustPlatform;
-          };
-          zifu = callPackage ./zifu {
-            inherit rustPlatform;
-            cargoHash = "sha256-bQMFznDsAsF2KhTryry2eLImtBdDS6/27/DB4OzSejI=";
-            source = sources.zifu;
-          };
-        }
-        // lib.optionalAttrs (pkgs.stdenv.isLinux) {
-          blender3 = pkgsB3d3.callPackage ./blender3 {
-            hash = "sha256-vFZjV6EDWARObGf0mWw/cQfILbWQtDQsML/bjyc4UJk=";
-            version = "3.6.23";
-          };
-          blender3-cpu = self'.packages.blender3.override {
-            cudaSupport = false;
-            hipSupport = false;
-          };
-          blender3-gpu = self'.packages.blender3.override {
-            cudaSupport = true;
-            hipSupport = true;
-          };
+      packages = {
+        alcom = builtins.warn "github:ms0503/pkgs.nix#alcom is deprecated. Please use nixpkgs#alcom instead." alcom;
+        discord-canary-wayland = callPackage ./discord-canary-wayland { };
+        ds4pairer = callPackage ./ds4pairer {
+          source = sources.ds4pairer;
         };
+        fakevrchat = callPackage ./fakevrchat { };
+        git-vrc = callPackage ./git-vrc {
+          inherit rustPlatform;
+          cargoHash = "sha256-/vO8xkD0uW0kqF8RzvAw2/TAvmDI5N8GZD0f6S6lY+M=";
+          source = sources.git-vrc;
+        };
+        microsoft-edge-dev = callPackage ./microsoft-edge-dev { };
+        microsoft-edge-dev-wayland = callPackage ./microsoft-edge-dev-wayland {
+          inherit (self'.packages) microsoft-edge-dev;
+        };
+        microsoft-edit = callPackage ./microsoft-edit {
+          inherit rustPlatform;
+          cargoHash = "sha256-U8U70nzTmpY6r8J661EJ4CGjx6vWrGovu5m25dvz5sY=";
+          source = sources.microsoft-edit;
+        };
+        noto-fonts-cjk-sans-non-variable = callPackage ./noto-fonts-cjk-sans-non-variable {
+          source = sources.noto-cjk-sans;
+        };
+        noto-fonts-cjk-serif-non-variable = callPackage ./noto-fonts-cjk-serif-non-variable {
+          source = sources.noto-cjk-serif;
+        };
+        noto-fonts-non-variable = callPackage ./noto-fonts-non-variable {
+          source = sources.noto-fonts;
+        };
+        proton-ge-rtsp-bin = callPackage ./proton-ge-rtsp-bin {
+          source = sources.proton-ge-rtsp-bin;
+        };
+        slack-wayland = callPackage ./slack-wayland { };
+        spotify-wayland = callPackage ./spotify-wayland { };
+        unzip-unicode = callPackage ./unzip-unicode { };
+        urxvt-wrapper = callPackage ./urxvt-wrapper { };
+        walland = callPackage ./walland {
+          source = sources.walland;
+        };
+        wezimgcat-wrapper = callPackage ./wezimgcat-wrapper {
+          inherit rustPlatform;
+        };
+        zifu = callPackage ./zifu {
+          inherit rustPlatform;
+          cargoHash = "sha256-bQMFznDsAsF2KhTryry2eLImtBdDS6/27/DB4OzSejI=";
+          source = sources.zifu;
+        };
+      }
+      // lib.optionalAttrs (pkgs.stdenv.isLinux) {
+        blender3 = pkgsB3d3.callPackage ./blender3 {
+          hash = "sha256-vFZjV6EDWARObGf0mWw/cQfILbWQtDQsML/bjyc4UJk=";
+          version = "3.6.23";
+        };
+        blender3-cpu = self'.packages.blender3.override {
+          cudaSupport = false;
+          hipSupport = false;
+        };
+        blender3-gpu = self'.packages.blender3.override {
+          cudaSupport = true;
+          hipSupport = true;
+        };
+      }
+      // generateExternalPackages externalPackages;
     };
 }
