9b39a9c337150e62a09a5bfed8f0521c84c84edb
Sora Tonami <ms0503@outlook.com>
2026-05-23 11:58:03 +0900

    fix: microsoft-edit: fail to build due to miss icu

--- pkgs/microsoft-edit/default.nix
+++ pkgs/microsoft-edit/default.nix
@@ -1,13 +1,21 @@
 {
   cargoHash,
+  icu,
   lib,
+  makeWrapper,
   mold,
   rustPlatform,
   source,
 }:
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage (finalAttrs: {
   inherit cargoHash;
   inherit (source) pname src version;
+  EDIT_CFG_ICU_EXPORT_SUFFIX = "_${lib.versions.major icu.version}";
+  LD_LIBRARY_PATH =
+    [
+      icu
+    ]
+    |> lib.makeLibraryPath;
   RUSTFLAGS = "-Clink-arg=-fuse-ld=mold";
   meta = {
     description = "A simple editor for simple needs";
@@ -20,6 +28,11 @@ rustPlatform.buildRustPackage {
     ];
   };
   nativeBuildInputs = [
+    makeWrapper
     mold
   ];
-}
+  postInstall = ''
+    wrapProgram "$out/bin/edit" \
+      --prefix LD_LIBRARY_PATH : "${finalAttrs.LD_LIBRARY_PATH}"
+  '';
+})
