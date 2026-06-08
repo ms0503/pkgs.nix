fc34203312b9ee7f8150215c7d54abf64ff9ba98
Sora Tonami <ms0503@outlook.com>
2026-06-09 00:54:02 +0900

    chore: add nix config

--- flake.nix
+++ flake.nix
@@ -129,6 +129,25 @@
       url = "github:ms0503/urlencode";
     };
   };
+  nixConfig = {
+    experimental-features = [
+      "flakes"
+      "nix-command"
+      "pipe-operators"
+    ];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://ms0503.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://nixpkgs-unfree.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "ms0503.cachix.org-1:Cc2mXpepZr7O9aFcRj5jq3mIcvdUPp85sLFuQj+IKbM="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+    ];
+  };
   outputs =
     inputs@{
       flake-parts,
