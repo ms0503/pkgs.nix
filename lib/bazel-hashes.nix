2ee8782e5e49a7d481541efa1591024c039be1e0
Sora Tonami <ms0503@outlook.com>
2026-09-14 05:52:19 +0900

    ci,fix: update hashes imperfectly

--- lib/bazel-hashes.nix
+++ lib/bazel-hashes.nix
@@ -1,4 +1,4 @@
 {
-  fcitx5-mozkey = "sha256-/LEV84Z4eXGQekmn4J1LLqRdSY2my0oac3uvKJ6zDNk=";
+  fcitx5-mozkey = "sha256-njZfRowJI8OCuKeQFniaeJhnsbNeEPdJLuCYeoZZZVQ=";
   mozkey = "sha256-/LEV84Z4eXGQekmn4J1LLqRdSY2my0oac3uvKJ6zDNk=";
 }
